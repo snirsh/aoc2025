b7cc6a6129ee0fb93eef92efb43f6904e5379513
Snir Sharristh <snirs@wix.com>
2025-12-02 07:40:00 +0200

    d2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ pnpm start 1
 
 ```
 Day 01
-Time part 1: 1.61ms
-Time part 2: 1.612ms
-Both parts: 3.222ms
+Time part 1: 3.092ms
+Time part 2: 1.784ms
+Both parts: 4.876ms
 ```
 
 ```
 Day 02
-Time part 1: -
+Time part 1: 171.492ms
 Time part 2: -
-Both parts: -
+Both parts: 171.492ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 3.222ms
+Total stars: 3/50
+Total time: 176.368ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.js
+++ src/day02/index.js
@@ -1,26 +1,54 @@
 import run from "aocrunner"
 
-const parseInput = (rawInput) => rawInput
+const parseInput = (rawInput) => rawInput.split(',').map(coup => coup.split('-')).filter(([c1, c2]) => !c1.startsWith('0') && !c2.startsWith('0'))
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
+  let inv = 0
+  for (const [c1, c2] of input) {
+    // console.log(c1, c2)
+    for (let i = +c1; i <= +c2; i++) {
+      const coup = i.toString()
+      // console.log(coup)
+      const mid = coup.length / 2
+      const left = coup.slice(0, mid)
+      const right = coup.slice(mid)
+      if (left === right) {
+        inv += +coup
+      }
+    }
+  }
 
-  return
+  return inv
 }
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  let inv = 0
+  for (const [c1, c2] of input) {
+    console.log(c1, c2)
+    for (let i = +c1; i <= +c2; i++) {
+      const coup = i.toString()
+      console.log(coup)
+      if ((coup + coup).slice(1, -1).includes(coup)) {
+        console.log('+', coup)
+        inv+=(+coup)
+      }
+    }
+  }
 
-  return
+  return inv
 }
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `11-22,99-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124`,
+        expected: 1227775554,
+      },
     ],
     solution: part1,
   },
